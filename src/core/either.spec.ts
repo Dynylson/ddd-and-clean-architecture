2169bc055f000c9a18e03507e42ce88969d51273
Dyn Júnior <dynylsonjr@gmail.com>
2025-02-20 22:40:03 -0300

    test: adicionar testes para o either

---
+++ src/core/either.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from 'vitest'
+import { Either, left, right } from './either'
+
+function doSomething(shouldSuccess: boolean): Either<string, number> {
+  if (shouldSuccess) {
+    return right(10)
+  } else {
+    return left('error')
+  }
+}
+
+test('success result', () => {
+  const result = doSomething(true)
+
+  if (result.isRight()) {
+    console.log(result.value)
+  }
+
+  expect(result.isRight()).toBe(true)
+  expect(result.isLeft()).toBe(false)
+})
+
+test('error result', () => {
+  const result = doSomething(false)
+
+  expect(result.isLeft()).toEqual(true)
+  expect(result.isRight()).toEqual(false)
+})
